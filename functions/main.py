e8011aeec17fd42f60355a27b5fc6376211642f7
Taco Vader <taco.vader@crisp-e.com>
2024-04-15 21:22:57 +0200

    Set region, fail parameters

--- functions/main.py
+++ functions/main.py
@@ -4,24 +4,27 @@
 
 from firebase_functions import https_fn
 from firebase_admin import initialize_app
+from firebase_functions.params import StringParam
+
+TELEGRAM_API_TOKEN = StringParam('TELEGRAM_API_TOKEN')
+SECRET_TOKEN = StringParam('SECRET_TOKEN')
 
 initialize_app()
 
 from telebot import TeleBot, types
 
-bot = TeleBot('TOKEN TODO') 
+bot = TeleBot(TELEGRAM_API_TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
 	bot.reply_to(message, "Howdy, how are you doing?")
 
-@https_fn.on_request()
-def on_request_example(req: https_fn.Request) -> https_fn.Response:
+@https_fn.on_request(region="europe-west3")
+def handle_webhook_callback(req: https_fn.Request) -> https_fn.Response:
     # Borrowing heavily from example at
     # https://github.com/eternnoir/pyTelegramBotAPI/blob/master/examples/multibot/main.py
     
-    # TODO: load correct token from .env, for now it is hardcoded to 'myTest'
-    if req.headers.get('X-Telegram-Bot-Api-Secret-Token') != 'myTest':
+    if req.headers.get('X-Telegram-Bot-Api-Secret-Token') != SECRET_TOKEN:
         print("No or incorrect token sent!")
         code403 = https_fn.FunctionsErrorCode('permission-denied')
         raise https_fn.HttpsError(code403, "Pass you shall not")
--- functions/setWebhook.sh
+++ functions/setWebhook.sh
@@ -4,5 +4,5 @@ if [ -z ${APP_URL+x} ] || [ -z ${TELEGRAM_API_TOKEN+x} ]
 then
     echo "Please set APP_URL and TELEGRAM_API_TOKEN in .env, eg APP_URL=https://telegram-ckjew3asd1.a.run.app etc"
 else 
-    curl -d "url=${APP_URL}&secret_token=myTest" "https://api.telegram.org/bot${TELEGRAM_API_TOKEN}/setWebhook"
+    curl -d "url=${APP_URL}&secret_token=${SECRET_TOKEN}" "https://api.telegram.org/bot${TELEGRAM_API_TOKEN}/setWebhook"
 fi
