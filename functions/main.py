ce5c368150ad86a5e70b67dcdcf19c693afb535c
TacoV <TacoV@users.noreply.github.com>
2024-04-16 14:02:22 +0000

    Why wont it work

--- functions/main.py
+++ functions/main.py
@@ -6,25 +6,30 @@ from firebase_functions import https_fn
 from firebase_admin import initialize_app
 from firebase_functions.params import StringParam
 
-TELEGRAM_API_TOKEN = StringParam('TELEGRAM_API_TOKEN')
-SECRET_TOKEN = StringParam('SECRET_TOKEN')
+TELEGRAM_API_TOKEN = StringParam('TELEGRAM_API_TOKEN').value
+SECRET_TOKEN = StringParam('SECRET_TOKEN').value
 
 initialize_app()
 
 from telebot import TeleBot, types
 
-bot = TeleBot(TELEGRAM_API_TOKEN.value)
+bot = TeleBot(TELEGRAM_API_TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+async def echo_message(message):
+    await bot.reply_to(message, message.text)
 
 @https_fn.on_request(region="europe-west3")
 def handle_webhook_callback(req: https_fn.Request) -> https_fn.Response:
     # Borrowing heavily from example at
     # https://github.com/eternnoir/pyTelegramBotAPI/blob/master/examples/multibot/main.py
-    
-    if req.headers.get('X-Telegram-Bot-Api-Secret-Token') != SECRET_TOKEN.value:
+
+    if req.headers.get('X-Telegram-Bot-Api-Secret-Token') != SECRET_TOKEN:
         print("No or incorrect token sent!")
         code403 = https_fn.FunctionsErrorCode('permission-denied')
         raise https_fn.HttpsError(code403, "Pass you shall not")
@@ -34,12 +39,12 @@ def handle_webhook_callback(req: https_fn.Request) -> https_fn.Response:
         code500 = https_fn.FunctionsErrorCode('unimplemented')
         raise https_fn.HttpsError(code500, "We need application/json data")
 
-    print("Parsing the update")
     json_string = req.get_data().decode('utf-8')
-    print("Found:", json_string)
+    print("Incoming update:", json_string)
     update = types.Update.de_json(json_string)
 
-    print("Processing the update")
+    print("Processing the update", update.message)
     bot.process_new_updates([update])
+    print("Processed the update")
 
     return ''
