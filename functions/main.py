0b6d41f05c8059ef62f9fd8f6d4ecca7354d054f
TacoV <TacoV@users.noreply.github.com>
2024-04-17 14:39:14 +0000

    Upgrade prio and report to telegram user

--- functions/main.py
+++ functions/main.py
@@ -1,5 +1,5 @@
 from firebase_admin import initialize_app
-from firebase_functions import https_fn, params
+from firebase_functions import https_fn, params, scheduler_fn
 from telebot import TeleBot, types
 from todoist_api_python.api import TodoistAPI
 
@@ -10,6 +10,7 @@ initialize_app()
 TELEGRAM_API_TOKEN = params.StringParam('TELEGRAM_API_TOKEN').value
 TELEGRAM_SECRET = params.StringParam('TELEGRAM_SECRET').value
 TODOIST_TOKEN = params.StringParam('TODOIST_TOKEN').value
+TELEGRAM_USER_ID = params.StringParam('TELEGRAM_USER_ID').value
 
 @https_fn.on_request(region="europe-west3")
 def telegram_webhook(req: https_fn.Request) -> https_fn.Response:
@@ -51,3 +52,32 @@ def todoist_webhook(req: https_fn.Request) -> https_fn.Response:
         )
 
     return ''
+
+# Manually run the task here https://console.cloud.google.com/cloudscheduler
+@scheduler_fn.on_schedule(region="europe-west3", schedule="every day 00:15")
+def nightly_prio_upgrade(event: scheduler_fn.ScheduledEvent) -> None:
+
+    api = TodoistAPI(TODOIST_TOKEN)
+    
+    message = "Increased priority:\n\n"
+
+    def describe_task(task):
+        prio_ball = { 1: '⚪', 2: '🔵', 3: '🟠', 4: '🔴'}
+        project = api.get_project(task.project_id)
+        return "" + project.name + ": " + prio_ball[task.priority] + " " + task.content + "\n"
+
+    for task in api.get_tasks(filter='(overdue|today)&!p1&!shared'):
+        task.priority = task.priority + 1
+        api.update_task(
+            task_id=task.id,
+            priority=task.priority
+        )
+        message += describe_task(task)
+
+    bot = TeleBot(TELEGRAM_API_TOKEN)
+
+    if message == "":
+        bot.send_message(TELEGRAM_USER_ID, "No due tasks to prioritize" )
+    else:
+        bot.send_message(TELEGRAM_USER_ID, message)
+
