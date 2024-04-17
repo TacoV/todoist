740a17464986b975caa9091bc23c0638af616988
TacoV <TacoV@users.noreply.github.com>
2024-04-17 13:07:05 +0000

    Working Telegram webhook

--- functions/main.py
+++ functions/main.py
@@ -7,15 +7,8 @@ initialize_app()
 TELEGRAM_API_TOKEN = params.StringParam('TELEGRAM_API_TOKEN').value
 SECRET_TOKEN = params.StringParam('SECRET_TOKEN').value
 
-bot = TeleBot(TELEGRAM_API_TOKEN)
-
-@bot.message_handler(func=lambda message: True)
-def handle_all_messages(message):
-    print("handle_all_messages")
-    bot.reply_to(message, "ACK")
-
 @https_fn.on_request(region="europe-west3")
-def handle_webhook_callback(req: https_fn.Request) -> https_fn.Response:
+def telegram_webhook(req: https_fn.Request) -> https_fn.Response:
     # https://github.com/eternnoir/pyTelegramBotAPI/blob/master/examples/webhook_examples/webhook_flask_echo_bot.py
 
     if req.headers.get('X-Telegram-Bot-Api-Secret-Token') != SECRET_TOKEN:
@@ -26,9 +19,15 @@ def handle_webhook_callback(req: https_fn.Request) -> https_fn.Response:
         print('Incorrect content-type')
         return https_fn.Response(status=403)
 
+    bot = TeleBot(TELEGRAM_API_TOKEN)
+
+    @bot.message_handler(func=lambda message: True)
+    def handle_all_messages(message:types.Message):
+        print("handle_all_messages called")
+        bot.send_message(message.chat.id, "Message received: "+message.text)
+
     json_string = req.get_data().decode('utf-8')
     update = types.Update.de_json(json_string)
-
     bot.process_new_updates([update])
 
     return ''
